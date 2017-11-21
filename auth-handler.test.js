55a5a95f291257ef9657d3dd3ac78b9e0b7528e7
Himanshu Pant <himanshu.pn3@gmail.com>
2017-11-21 13:28:47 -0800

    sanity check

--- auth-handler.test.js
+++ auth-handler.test.js
@@ -1,5 +1,5 @@
 const { authHandler } = require("./auth-handler");
-
+// sanity check
 describe("authHandler", () => {
   it("should exist", () => {
     expect(typeof authHandler).toBe("function");
